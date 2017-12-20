ccbb4e4ec3fbaf52425bc70c77bec3b913f7e678
Abhishek Srivastava <abhishek8developer@gmail.com>
2017-12-20 15:21:57 +0530

    added author, date, type filters working

--- src/modules/search/components/SearchEngine.js
+++ src/modules/search/components/SearchEngine.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import InputText from 'components/generic/input/InputText';
 import {Button} from 'components/generic/buttons';
 
-const SearchEngine = ({onchange, author, date, type}) => {
+const SearchEngine = ({onInputchange, onAuthorchange, onDatechange, onSearchTypechange, author, date, type}) => {
   return (
     <div className="row">
       <div className="col-sm-3 col-md-2">
@@ -13,19 +13,19 @@ const SearchEngine = ({onchange, author, date, type}) => {
       </div>
       <div className="col-sm-6 well">
         <div className="searchInput">
-          <InputText name="searchString" onchange={onchange} placeHolder="Search for issues" className='col-sm-8' style={{'position': 'relative', 'float': 'left'}} />
+          <InputText name="searchString" onchange={onInputchange} placeHolder="Search for issues" className='col-sm-8' style={{'position': 'relative', 'float': 'left'}} />
         </div>
         <Button className='inline-button' type="submit" label="Search" />
         <div className="filters">
-          <select name='author'>
+          <select name='author' onChange={onAuthorchange}>
             <option value=''>-- Please Select --</option>
             {author.map((value, key) => (<option key={key} value={value}>{value}</option>))}
           </select>
-          <select name='date'>
+          <select name='date' onChange={onDatechange}>
             <option value=''>-- Please Select --</option>
             {date.map((value, key) => (<option key={key} value={value}>{value}</option>))}
           </select>
-          <select name='type'>
+          <select name='type' onChange={onSearchTypechange}>
             <option value=''>-- Please Select --</option>
             {type.map((value, key) => (<option key={key} value={value}>{value}</option>))}
           </select>
