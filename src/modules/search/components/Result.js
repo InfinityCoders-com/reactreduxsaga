6bee82bfb476fb59976d2816502bed3ef99d870e
Abhishek Srivastava <abhishek8developer@gmail.com>
2017-12-21 11:51:37 +0530

    added frontend design

--- src/modules/search/components/Result.js
+++ src/modules/search/components/Result.js
@@ -16,12 +16,12 @@ const Result = ({data}) => {
         {data.map((value, key) => (
           <div key={key} className='result-item'>
             <div className='row'>
-              <h4 className="col-sm-6">{value.heading}</h4>
-              <h5 className="col-sm-3 p-t-sm">{value.author}</h5>
-              <h5 className="col-sm-3 p-t-sm">{value.type}</h5>
+              <h5 className="col-sm-6 text-info">{value.heading}</h5>
+              <h6 className="col-sm-3 p-t-xs author">{value.author}</h6>
+              <h6 className="col-sm-3 p-t-xs type">{value.type}</h6>
             </div>
-            <Link to={value.link}>{value.link}</Link>
-            <p>{value.date + ' - ' + value.description}</p>
+            <a href={value.link}><span className='text-success'>{value.link}</span></a>
+            <p className='desc'>{value.date + ' - ' + value.description}</p>
           </div>
         ))}
       </div>
