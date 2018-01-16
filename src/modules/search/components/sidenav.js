71e18527f67bd66ce96a52c96ddbd6140683b98e
InfinityCoders <infiniticoder@gmail.com>
2018-01-16 18:36:43 +0530

    added gmail login API

--- src/modules/search/components/sidenav.js
+++ src/modules/search/components/sidenav.js
@@ -1,6 +1,8 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import Checkbox from 'components/generic/input/checkbox';
+import GoogleLogin, {GoogleLogout} from 'react-google-login';
+
 
 const SideNav = ({searchWithin, searchWithinValue}) => {
   return (
@@ -9,6 +11,24 @@ const SideNav = ({searchWithin, searchWithinValue}) => {
       <ul className='sideNav'>
         <li><Checkbox onchange={searchWithin} value={searchWithinValue} label='Search Within Results' /></li>
       </ul>
+      <div>
+        <div>
+          <GoogleLogin
+            clientId="738838019990-6m0er1meljtdrua2i132tj3ml8q8e6ne.apps.googleusercontent.com"
+            buttonText="Login"
+            onSuccess={(e)=>{console.log(e,'asdasd');
+            }}
+            onFailure={(e)=>{console.log(e,'fail');
+            }}
+          />
+        </div>
+        <div>
+          {/* <GoogleLogout
+            buttonText="Logout"
+            onLogoutSuccess={()=>{console.log( 'logout')}}
+          /> */}
+        </div>
+      </div>
     </div>
   );
 };
